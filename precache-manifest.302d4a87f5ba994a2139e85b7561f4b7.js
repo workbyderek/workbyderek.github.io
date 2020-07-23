567f14d4c393128f7f0790901f4692df9c715515
Derek Palladino <derekbot@Dereks-MacBook-Pro.local>
2020-07-23 13:59:45 -0400

    Updates

---
+++ precache-manifest.302d4a87f5ba994a2139e85b7561f4b7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "16f4fb9501ab2e84823692e776c2e5ab",
+    "url": "/photography/index.html"
+  },
+  {
+    "revision": "3aabf086e3e60f9ec35e",
+    "url": "/photography/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "35e5ddba86f0a92a2c4f",
+    "url": "/photography/static/js/2.5f41f4a6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/photography/static/js/2.5f41f4a6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3aabf086e3e60f9ec35e",
+    "url": "/photography/static/js/main.01756a75.chunk.js"
+  },
+  {
+    "revision": "0c47e9bcbfd69fd1583a",
+    "url": "/photography/static/js/runtime-main.7e82a340.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/photography/static/media/logo.5d5d9eef.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.69b4c43c905f8234d0f3111e5df6f8b4.js"
+  "/photography/precache-manifest.302d4a87f5ba994a2139e85b7561f4b7.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/photography/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
